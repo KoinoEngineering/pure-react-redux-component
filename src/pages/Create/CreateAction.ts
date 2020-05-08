917b8b5efd9adf0e4d8463326c0e39df96b396aa
KoinoEngineering <email@koino.engineering>
2020-05-08 10:06:07 +0900

    #4 fix submitting

--- src/pages/Create/CreateAction.ts
+++ src/pages/Create/CreateAction.ts
@@ -1,28 +1,16 @@
 import { Action } from "redux";
-import { Payload } from "src/interfaces/Action";
-import { CreateState } from "./CreateReducer";
 
 export enum ActionType {
-    CHANGE_VALUES = "blog-sample/create/CHANGE_VALUES",
     SUBMIT = "blog-sample/create/SUBMIT",
-    SUBMIT_END = "blog-sample/create/SUBMIT_END",
 }
 
-export interface ChangeValuesAction extends Action<ActionType.CHANGE_VALUES>, Payload<Partial<CreateState>> { }
 export interface SubmitAction extends Action<ActionType.SUBMIT> { }
-export interface SubmitEndAction extends Action<ActionType.SUBMIT_END> { }
-
 
 export type CreateActions =
-    ChangeValuesAction |
-    SubmitAction |
-    SubmitEndAction;
-
+    SubmitAction;
 
 const createActionCreators = {
-    changeValues: (payload: ChangeValuesAction["payload"]): ChangeValuesAction => ({ type: ActionType.CHANGE_VALUES, payload }),
     submit: (): SubmitAction => ({ type: ActionType.SUBMIT }),
-    submitEnd: (): SubmitEndAction => ({ type: ActionType.SUBMIT_END })
 };
 
 export default createActionCreators;
--- src/pages/Create/CreateReducer.ts
+++ src/pages/Create/CreateReducer.ts
@@ -1,31 +1,14 @@
 import { Reducer } from "redux";
-import { ActionType, CreateActions } from "./CreateAction";
+import { CreateActions } from "./CreateAction";
 
 export interface CreateState {
-    submitting: boolean;
 }
 
 const initialState = (): CreateState => ({
-    submitting: false,
 });
 
 const create: Reducer<CreateState, CreateActions> = (state = initialState(), action) => {
     switch (action.type) {
-        case ActionType.CHANGE_VALUES:
-            return {
-                ...state,
-                ...action.payload
-            };
-        case ActionType.SUBMIT:
-            return {
-                ...state,
-                submitting: true
-            };
-        case ActionType.SUBMIT_END:
-            return {
-                ...state,
-                submitting: false
-            };
         default:
             return state;
     }
